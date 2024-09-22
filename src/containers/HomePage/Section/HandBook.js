48429ad97f0b59a678ac0be76a5b00b0fbd68ffa
Khai Do <khaidooo192@gmail.com>
2024-09-23 02:02:48 +0700

    homepage clone

---
+++ src/containers/HomePage/Section/HandBook.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { FormattedMessage } from 'react-intl';
+import Slider from 'react-slick';
+
+
+
+class HandBook extends Component {   
+    render() {
+       
+        return (
+           <div className='section-share section-handbook'>
+                <div className='section-container'>
+                    <div className='section-header'>
+                        <span className='title-section'>Cẩm nang</span>
+                        <button className='btn-section'>Xem thêm</button>
+                    </div>
+                    <div className='section-body'>
+                        <Slider {...this.props.settings}>
+                            <div className='section-custommize'>
+                                <div  className='bg-image section-handbook'></div>
+                                <div>Top bệnh thường gặp mùa mưa 1</div>
+                            </div>
+                            <div className='section-custommize'>
+                                <div  className='bg-image section-handbook'></div>
+                                <div>Top bệnh thường gặp mùa mưa 2</div>
+                            </div>
+                            <div className='section-custommize'>
+                                <div  className='bg-image section-handbook'></div>
+                                <div>Top bệnh thường gặp mùa mưa 3</div>
+                            </div>
+                            <div className='section-custommize'>
+                                <div  className='bg-image section-handbook'></div>
+                                <div>Top bệnh thường gặp mùa mưa 4</div>
+                            </div>
+                            <div className='section-custommize'>
+                                <div  className='bg-image section-handbook'></div>
+                                <div>Top bệnh thường gặp mùa mưa 5</div>
+                            </div>
+                            <div className='section-custommize'>
+                                <div  className='bg-image section-handbook'></div>
+                                <div>Top bệnh thường gặp mùa mưa 6</div>
+                            </div>
+                            
+                        </Slider>
+                    </div>
+                    
+                </div>
+            </div>
+        );
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+        isLoggedIn: state.user.isLoggedIn,
+        language: state.app.language,
+
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HandBook);
